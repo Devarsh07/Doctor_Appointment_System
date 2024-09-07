5754e9c624e4cf6a3a788bcd49605cc9e6667dbf
Devarsh07 <debarshupadhyay7@gmail.com>
2024-09-07 17:16:04 +0530

    new commit

--- index.js
+++ index.js
@@ -17,12 +17,20 @@ connectDb();
 const app = express();
 
 // middlewares
-app.use(cors({
-  origin: "https://doctor-appointment-system-ochre.vercel.app",
-  methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: "Content-Type,Authorization",
-  credentials: true 
-}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://doctor-appointment-system-ochre.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  
+  // Handle preflight requests
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  
+  next();
+});
+
 
 app.use(express.json());
 app.use(morgan("dev"));

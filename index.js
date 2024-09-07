fc69de9b6c9cb55774e252ed4619f785ab662d1c
Devarsh07 <debarshupadhyay7@gmail.com>
2024-09-07 17:01:39 +0530

    fourth commit

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
 const express = require("express");
 const colors = require("colors");
-const moragan = require("morgan");
+const morgan = require("morgan");
 const dotenv = require("dotenv");
+const cors = require("cors"); // Add this line
 const userRoutes = require('./routes/userRoute');
 const adminRoutes = require("./routes/adminRoute");
-const doctorRoutes = require("./routes/doctorRoute")
+const doctorRoutes = require("./routes/doctorRoute");
 const connectDb = require("./config/connectDb");
 const path = require("path");
 
-//dotenv conig
+// dotenv config
 dotenv.config();
 connectDb();
 
-//rest obejct
+// rest object
 const app = express();
 
-//middlewares
+// middlewares
+app.use(cors({ origin: "https://doctor-appointment-system-ochre.vercel.app" })); // Enable CORS for your frontend domain
 app.use(express.json());
-app.use(moragan("dev"));
+app.use(morgan("dev"));
 
-//routes
+// routes
 app.use("/api/user", userRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/doctor", doctorRoutes);
 
-//static files
+// static files
 app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-//port
+// port
 const PORT = process.env.PORT || 4000;
-//listen port
+// listen port
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
